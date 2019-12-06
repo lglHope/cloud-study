e6b0ad68e143208ae089fe99e6bfaa9de228b143
lglHope <l1911739019@qq.com>
2019-12-06 16:59:45 +0800

    redis操作命令和配置详解

--- redis-6379/src/test/java/liu/hope/redis6379/Redis6379ApplicationTests.java
+++ redis-6379/src/test/java/liu/hope/redis6379/Redis6379ApplicationTests.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -20,6 +23,17 @@ public class Redis6379ApplicationTests {
     public void contextLoads() {
         // 一次操作一个事务
         redisTemplate.opsForValue().set("name", "小胡");
+
+        // 多次操作一个事务
+        String name = (String) redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                redisTemplate.opsForValue().set("name2","小胡");
+                String name2 = (String) redisTemplate.opsForValue().get("name2");
+                return name2;
+            }
+        });
+        System.out.println("name2:" + name);
     }
 
 }
