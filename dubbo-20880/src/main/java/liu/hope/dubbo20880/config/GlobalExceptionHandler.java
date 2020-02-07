e9f196e30f002f9896afaacad2517265452410bd
lglHope <l1911739019@qq.com>
2020-02-07 18:36:52 +0800

    web异常统一处理

---
+++ dubbo-20880/src/main/java/liu/hope/dubbo20880/config/GlobalExceptionHandler.java
@@ -0,0 +1,103 @@
+package liu.hope.dubbo20880.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 统一异常处理的类
+ */
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    private static final String DEFAULT_ERROR_VIEW = "error";
+
+    /**
+     * 异常处理跳转到异常页面
+     * @param req
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
+    }
+
+    /**
+     * 异常处理返回json
+     * @param req
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(value = MyException.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, MyException e) throws Exception {
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setMessage(e.getMessage());
+        r.setCode(ErrorInfo.ERROR);
+        r.setData("Some Data");
+        r.setUrl(req.getRequestURL().toString());
+        return r;
+    }
+}
+
+class MyException extends Exception{
+    public MyException(String message){
+        super(message);
+    }
+}
+
+class ErrorInfo<T> {
+
+    public static final Integer OK = 0;
+    public static final Integer ERROR = 100;
+
+    private Integer code;
+    private String message;
+    private String url;
+    private T data;
+
+    // 省略getter和setter
+
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
